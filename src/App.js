dd81698b47e4fc36d844a31825dd25299dc0c46b
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-09 15:33:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SignupForm } from "./Components/SignupForm/SignupForm";
+
+const containerStyles = {
+  maxWidth: 1170,
+  marginLeft: "auto",
+  marginRight: "auto",
+  paddingLeft: 15,
+  paddingRight: 15,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyles}>
+      <SignupForm />
     </div>
   );
 }
---
+++ src/Components/SignupForm/SignupForm.js
@@ -0,0 +1,89 @@
+// import useLocalStorage from "../../hooks/useLocalStorage";
+import styles from "./SignupForm.module.css";
+import { useState } from "react";
+
+export function SignupForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //   console.log(email);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+
+      case "password":
+        setPassword(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  return (
+    <form className={styles.form} autoComplete="off">
+      <label className={styles.label}>
+        <span>Email</span>
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
+      </label>
+      <label className={styles.label}>
+        <span>Пароль</span>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+        />
+      </label>
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
+}
+
+// export class OldSignupForm extends Component {
+//   state = {
+//     email: "",
+//     password: "",
+//   };
+//   handleChange = (evt) => {
+//     const { name, value } = evt.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     return (
+//       <form className={styles.form} autoComplete="off">
+//         <label className={styles.label}>
+//           <span>Почта</span>
+//           <input
+//             type="email"
+//             name="email"
+//             onChange={this.handleChange}
+//             value={this.state.email}
+//           />
+//         </label>
+
+// <label className={styles.label}>
+//   <span>Пароль</span>
+//   <input
+//     type="password"
+//     name="password"
+//     onChange={this.handleChange}
+//     value={this.state.password}
+//   />
+// </label>
+
+//         <button type="submit">Зарегистрироваться</button>
+//       </form>
+//     );
+//   }
+// }
